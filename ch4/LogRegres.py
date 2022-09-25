fbe1d705645a91c29bfef581f72982f071beb25c
Lyn4444 <3077002706@qq.com>
2022-09-25 22:25:10 +0800

    Logistic

--- ch4/LogRegres.py
+++ ch4/LogRegres.py
@@ -17,6 +17,7 @@
 
 """
 import math
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -39,7 +40,11 @@ def loadDataSet():
 # Sigmoid函数（阶越函数）
 # f(Z) = 1 / (1 + e^(-z))
 def sigmoid(inX):
-    return 1.0 / (1 + np.exp(-inX))
+    #  #对sigmoid函数的优化，避免了出现极大的数据溢出
+    if inX >= 0:
+        return 1.0 / (1 + np.exp(-inX))
+    else:
+        return np.exp(inX) / (1 + np.exp(inX))
 
 
 # 梯度上升法
@@ -66,8 +71,8 @@ def gradAscend(dataMatIn, classLabels):
 
 # 画出数据集和logistic回归最佳拟合直线的函数
 def plotBestFit(weight):
-    # getA(): 将矩阵转成数组的形式
-    weights = weight.getA()
+    # weights = weight.getA()
+    weights = weight
     dataMat, labelMat = loadDataSet()
     dataArray = np.array(dataMat)
     n = np.shape(dataArray)[0]
@@ -95,10 +100,104 @@ def plotBestFit(weight):
     plt.show()
 
 
+# 随机梯度上升算法；一次仅用一个样本点来更新回归系数
+# 区别：
+#   1.梯度上升算法的变量h和误差error都是向量,随机梯度上升算法则全是数值型
+#   2.随机梯度上升算法没有矩阵的转换过程，所有变量的数据类型都是NumPy数组
+def stoGradAscent0(dataMat, classLabels):
+    m, n = np.shape(dataMat)
+    alpha = 0.01
+    weight = np.ones(n)
+    for i in range(m):
+        h = sigmoid(np.sum(dataMat[i] * weight))
+        error = classLabels[i] - h
+        weight = weight + alpha * error * dataMat[i]
+    return weight
+
+
+# 随机梯度上升算法的回归系数经过⼤量迭代才能达到稳定值，并且仍然有局部的波动现象
+# 原因：存在⼀些不能正确分类的样本点（数据集并⾮线性可分），在每次迭代时会引发系数的剧烈改变。我们期望算法能避免来回波动，从⽽收敛到某个值,同时收敛速度也需要加快。
+# 改进的随机梯度上升算法
+def stoGradAscent1(dataMat, classLabels, numIter=50):
+    m, n = np.shape(dataMat)
+    weights = np.ones(n)
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            # alpha每次迭代时都需要调整来缓解波动，常数项是为了保证在多次迭代之后新数据依旧有一定的影响力，如果处理的是动态数据则需要加大常数项来确保获得更大的回归系数
+            alpha = 4 / (1.0 + j + i) + 0.01
+            # 随机选取更新
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMat[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMat[randIndex]
+            del (dataIndex[randIndex])
+    return weights
+
+
+# Logistic回归估计病马的死亡率
+# 处理数据中的缺失值
+#   1.使用可用特征的均值来填补缺失值
+#   2.使用特殊值来填补，如-1
+#   3.忽略有缺失值的样本
+#   4.使用相似的样本均值来填补
+#   5.使用另外的机器学习算法来预测缺失值
+
+# 如果是数据的类别标签缺失则会把该数据丢弃
+# 以回归系数和特征向量作为输⼊来计算对应的Sigmoid值
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    fileTrain = open('horseColicTraining.txt')
+    fileTest = open('horseColicTest.txt')
+    trainSet = []
+    trainLabel = []
+    for line in fileTrain.readlines():
+        _line = line.strip().split('\t')
+        lineArray = []
+        for i in range(21):
+            lineArray.append(float(_line[i]))
+        trainSet.append(lineArray)
+        trainLabel.append(float(_line[21]))
+    trainWeghts = stoGradAscent1(np.array(trainSet), trainLabel, 500)
+    errorCount = 0
+    numTestVec = 0.0
+    for line in fileTest.readlines():
+        numTestVec += 1.0
+        _line = line.strip().split("\t")
+        lineArray = []
+        for i in range(21):
+            lineArray.append(float(_line[i]))
+        if int(classifyVector(np.array(lineArray), trainWeghts)) != int(_line[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("error rate is: " + str(errorRate))
+    return errorRate
+
+
+def multiTest():
+    numTest = 10
+    errorNum = 0.0
+    for k in range(numTest):
+        errorNum += colicTest()
+    print(str(numTest) + " iterations the rate is: " + str(errorNum / float(numTest)))
+
+
 if __name__ == "__main__":
     dataArr, labelsMat = loadDataSet()
-    weight = gradAscend(dataArr, labelsMat)
+    # weight = gradAscend(dataArr, labelsMat)
     # print(dataArr)
     # print(labelsMat)
-    print(weight.getA())
-    plotBestFit(weight)
+    # getA(): 将矩阵转成数组的形式
+    # print(weight.getA())
+    # plotBestFit(weight.getA())
+    # weight = stoGradAscent1(np.array(dataArr), labelsMat, 5000)
+    # print(weight)
+    # plotBestFit(weight)
+    multiTest()
